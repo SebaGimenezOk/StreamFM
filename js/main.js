43c813a344c60459f8c8194c48bfb520ce1c6bca
SebaGimenez <sebastiangimenezlopezen@gmail.com>
2022-07-20 22:38:36 -0300

    primer commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+const audio = document.querySelector(".audio");
+const playButton = document.querySelector(".play");
+const playButtonIcon = playButton.querySelector("i")
+const stopButton = document.querySelector(".stop")
+const externalButton = document.querySelector(".external")
+
+audio.addEventListener("click", playPauseAudio)
+playButton.addEventListener("click", playPauseAudio)
+stopButton.addEventListener("click", stopAudio)
+
+
+function playPauseAudio() {
+
+    audio[audio.paused ? "play" : "pause"]()
+    playButtonToggleIcon()
+
+}
+
+
+function playButtonToggleIcon() {
+    if (audio.paused) {
+        playButtonIcon.classList.remove("fa-pause")
+        playButtonIcon.classList.add("fa-play")
+    } else {
+        playButtonIcon.classList.remove("fa-play")
+        playButtonIcon.classList.add("fa-pause")
+    }
+
+}
+
+function stopAudio() {
+    audio.pause()
+    audio.currentTime = 0
+    playButtonToggleIcon()
+}
